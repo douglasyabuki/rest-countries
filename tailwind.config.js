925488bf0b8904dfc7d8dbeda970c30e65d276df
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-02-07 14:42:49 -0300

    Commit 7: Theme colors configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
+    colors:{
+      'dark-mode-element':'hsl(209, 23%, 22%)',
+      'dark-mode-background':'hsl(207, 26%, 17%)',
+      'dark-mode-text':'hsl(0, 0%, 100%)',
+      'light-mode-element':'hsl(0, 0%, 100%)',
+      'light-mode-input':'hsl(0, 0%, 52%)',
+      'light-mode-background':'hsl(0, 0%, 98%)',
+      'light-mode-text':'hsl(200, 15%, 8%)',
+      'translucid-black': '#0000005e',
+      'transparent-black': ' #19181F80',
+    },
     extend: {},
   },
   plugins: [],
