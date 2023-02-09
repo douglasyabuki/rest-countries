3bb0f543e6259094663106c31dc2f9257f4ac291
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-02-09 13:05:22 -0300

    Commit 14: Bootstrap API

--- src/pages/api/countries.ts
+++ src/pages/api/countries.ts
@@ -5,9 +5,17 @@ type Data = {
   name: string
 }
 
+type ListData = {
+
+}
+
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data | ListData>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === 'GET') {
+    res.status(200).json({ name: 'LALALAL' })
+  } else {
+    res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
 }
