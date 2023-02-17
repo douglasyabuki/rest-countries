86199257427862462d69afa1967def79bed57ef4
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-02-17 10:29:58 -0300

    Commit 40: RegionFilter.tsx works

--- src/components/filterable-countries/filterable-bar/region-filter/RegionFilter.tsx
+++ src/components/filterable-countries/filterable-bar/region-filter/RegionFilter.tsx
@@ -7,9 +7,21 @@ interface Props {
   setFilterRegion: any;
 }
 
+// Region Filter main function
 export default function RegionFilter({ filterRegion, setFilterRegion }: Props) {
+
+  // Changes parent state (FilterableCountries.tsx) on user select
+  const onChangeHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterRegion(event.target.value);
+  };
+
+  // Returns the select tag with its options to FilterableBar.tsx
   return (
-    <select className="p-6 bg-light-mode-element text-light-mode-text dark:bg-dark-mode-element dark:text-dark-mode-text transition-all duration-300 rounded-lg">
+    <select
+      className="p-6 bg-light-mode-element text-light-mode-text dark:bg-dark-mode-element dark:text-dark-mode-text transition-all duration-300 rounded-lg"
+      onChange={onChangeHandler}
+      value={filterRegion}
+    >
       <option value="" disabled selected hidden>
         Filter by Region
       </option>
