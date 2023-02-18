394fca705de9776ebe30d11de8670d40f2127b80
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-02-18 09:42:36 -0300

    Commit 43: Add filter features to country cards

--- src/components/filterable-countries/country-cards/CountryCards.tsx
+++ src/components/filterable-countries/country-cards/CountryCards.tsx
@@ -6,20 +6,40 @@ import CountryCard from "./country-card/CountryCard";
 
 // Props destructuring
 export interface ICountryCardsProps {
-    countryList: ICountry[],
-    filterText: any,
-    filterRegion: any,
+  countryList: ICountry[];
+  filterText: any;
+  filterRegion: any;
 }
 
 // Country Cards main function
-export default function CountryCards ({countryList, filterText, filterRegion}: ICountryCardsProps) {
+export default function CountryCards({countryList, filterText, filterRegion}: ICountryCardsProps) {
+
+  // Creating an empty array which will receive only filtered countries
+  const filteredCountryList: ICountry[] = [];
+
+  // Function to populate the empty array based on filterText and filterRegion values
+  countryList.forEach((country) => {
+    if (
+      (country.name.common.toLowerCase().indexOf(filterText.toLowerCase()) &&
+        country.name.official.toLowerCase().indexOf(filterText.toLowerCase)) ===
+      -1
+    ) {
+      return;
+    }
+    if (
+      country.region.toLowerCase().indexOf(filterRegion.toLowerCase()) === -1
+    ) {
+      return;
+    }
+    filteredCountryList.push(country);
+  });
+
+  // Returns the filtered / to be filtered countries to FilterableCountries.tsx
   return (
     <div className={`flex flex-wrap justify-evenly gap-20`}>
-    {
-      countryList.map((item, id) => (
+      {filteredCountryList.map((item, id) => (
         <CountryCard key={id} country={item}></CountryCard>
-      ))
-    }
-  </div>
+      ))}
+    </div>
   );
 }
