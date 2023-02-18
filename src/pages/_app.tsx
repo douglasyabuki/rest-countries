e2a3d306fcde6119e37170c24578683da36bfe93
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-02-18 10:20:52 -0300

    Commit 45: Bootstrap details page features

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '@/styles/globals.css'
 import Layout from '@/components/layout/Layout';
 import type { AppProps } from 'next/app'
-import { Nunito_Sans } from '@next/font/google'
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,12 +1,3 @@
-// export async function getStaticProps() {
-//     const data = await fetch('')
-//     const details = await data.json()
-
-//     return{
-//         props: {details},
-//     }
-// }
-
 export default function Details({}){
     return <>hello</>
 }
