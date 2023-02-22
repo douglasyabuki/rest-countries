b15550185632b10f4b6646041b9869092a0fc808
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-02-22 08:57:18 -0300

    Commit 46: Separating imports on _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
-import '@/styles/globals.css'
-import Layout from '@/components/layout/Layout';
+// Next Native Components
 import type { AppProps } from 'next/app'
+
+// CSS
+import '@/styles/globals.css'
+
+// Icons
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
+
+// Page Layout
+import Layout from '@/components/layout/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
