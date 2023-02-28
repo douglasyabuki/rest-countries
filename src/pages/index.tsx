fae140b076c07ef04fba42bcf8483a1cd2caaaac
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-02-28 09:47:59 -0300

    Commit 55: Changing state owner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,33 @@
 import LoadingAnimation from "@/components/common/loading-animation/LoadingAnimation";
 import Countries from "@/components/filterable-countries/FilterableCountries";
 
+// Hooks
+import { useState, useEffect } from 'react'
+
 // Interfaces
 import { ICountry } from "@/interfaces/ICountry";
 
-// Props destructuring
-interface HomeProps {
-  countryList: ICountry[],
-  isLoading: boolean
-}
+// Utils
+import { fetchCountries } from '@/utils/fetchCountries';
 
 // Home main function
-export default function Home({countryList, isLoading}: HomeProps) {
+export default function Home() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [countryList, setCountryList] = useState<ICountry[] | null>(null);
 
+  // Function to fetch data from api/countries
+  const loadCountryList = async () => {
+    let list = await fetchCountries();
+    list.sort((a,b) => a.name.common.localeCompare(b.name.common));
+    setCountryList(list);
+    setIsLoading(false);
+    console.log(countryList);
+  };
+
+  // Triggers load on app start
+  useEffect(() => {
+    loadCountryList();
+  },[]);
 
   // This is the Home page. It passes the fetched data through props to Countries.tsx
   return (
